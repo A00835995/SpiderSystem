1180e942743ec87fd58b4ede14e478313fc624e4
EmilioVidal <149820545+EmilioVidal@users.noreply.github.com>
2025-06-04 21:00:51 -0600

    Modificación de GP

--- Front2/src/services/inventoryService.js
+++ Front2/src/services/inventoryService.js
@@ -34,6 +34,9 @@ export async function fetchTotalInventoryCount() {
             axiosInstance.get('/gettotalproductos')
         ]);
         
+        const totalData = totalResponse.data.data;
+        const statusData = statusResponse.data.data;
+        
         return {
             disponibles: statusData.Disponible || 0,
             bajoStock: statusData["Bajo Stock"] || 0,
